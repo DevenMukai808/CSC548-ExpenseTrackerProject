d8db6ff534c2f3d48c22e30fe8dfd7ef14ccbfb2
Cam Abanes <caa43510@creighton.edu>
2025-10-31 11:12:13 -0500

    turned into spring boot project

---
+++ expense_tracker/src/test/java/com/cameron_deven_kirtan/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cameron_deven_kirtan.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
